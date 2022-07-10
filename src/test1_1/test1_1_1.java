6ebebd992e4e9fdfa83e338bfa96fd50957645f2
777August <2308161781@qq.com>
2022-07-10 17:27:26 +0800

    try again

--- src/test1_1/test1_1_1.java
+++ src/test1_1/test1_1_1.java
@@ -3,5 +3,6 @@ package test1_1;
 public class test1_1_1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("try again");
     }
 }
