aa17e15727f09e455cbe37982228ab124e9edc9a
777August <2308161781@qq.com>
2022-07-17 00:12:21 +0800

    测试下idea连接GitHub是否成功

--- src/test1_1/test1_1_1.java
+++ src/test1_1/test1_1_1.java
@@ -4,5 +4,6 @@ public class test1_1_1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("try again");
+        System.out.println("idea直接提交");
     }
 }
