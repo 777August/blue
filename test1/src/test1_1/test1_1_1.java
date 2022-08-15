a8b351a74717bc227f7a54da7ea745ff77ba33a0
777August <2308161781@qq.com>
2022-08-15 11:01:35 +0800

    乱套了QAQ

--- test1/src/test1_1/test1_1_1.java
+++ test1/src/test1_1/test1_1_1.java
@@ -5,6 +5,7 @@ public class test1_1_1 {
         System.out.println("hello world");
         System.out.println("try again");
         System.out.println("idea直接提交");
+        System.out.println("完了完了乱套了");
         String s = "-123";
         System.out.println(s.length());
         s = s.substring(0,s.length()-1);
